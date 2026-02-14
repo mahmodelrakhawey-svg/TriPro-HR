4895933d01263559097e6999e2c3e8374a2b563e
mahmodelrakhawe <mahmodelrakhawey@gmail.com>
2026-02-14 23:11:01 +0200

    Fix Android build ProGuard error and update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tripro.hr',
-  appName: 'TriPro HR',
-  webDir: 'build',
-  server: {
-     url: 'http://192.168.2.44:3000',
-    androidScheme: 'https'
-  }
+  appId: 'com.example.app',
+  appName: 'TriPro-HR',
+  webDir: 'build', 
+  // server: {
+  //   url: 'http://192.168.2.44:3000', // رابط السيرفر المحلي الخاص بك
+  //   cleartext: true // <--- أضف هذا السطر للسماح بـ HTTP
+  // }
 };
 
 export default config;
